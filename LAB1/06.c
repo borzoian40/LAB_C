486112df9f2d24d3345bf8a7d3a5a38714046c48
Borzu <74207166+borzoian40@users.noreply.github.com>
2023-12-29 22:48:00 +0100

    Create 06.c

---
+++ LAB1/06.c
@@ -0,0 +1,17 @@
+/*
+Exercise 6. Starting from the code of Exercise 5, write a C program that reads a set of operations from 
+a text file, reported one per line. The program should create a second file, reporting in each 
+line the operator and the result of the corresponding operation in the input file. The result 
+should be written with two digits after the decimal point, as in the example below. 
+Example
+Input file: Operations.txt:
++ 15.225 30.51
+- 42.1 10.01
+* 0.62 2.4
+/ 5.0 2.5
+Output file: Results.txt:
++ 45.74
+- 32.09
+* 1.49
+/ 2.00
+*/
