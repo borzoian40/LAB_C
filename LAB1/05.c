8cdf782269c71f0753fc245f43f9b9da68494665
Borzu <74207166+borzoian40@users.noreply.github.com>
2023-12-29 23:32:30 +0100

    Create 05.c

---
+++ LAB1/05.c
@@ -0,0 +1,12 @@
+/*
+Exercise 5. Write a “calculator” in C, that is able to perform basic arithmetic operations (addition, subtraction, division and multiplication) on two given inputs op1 and op2.
+Write a C program that:
+a) Acquires a character from keyboard using getchar (either ‘+’, ‘-‘, ‘*’ or ‘/’) to decide 
+which should be the arithmetic operation to perform;
+b) Acquires two float operands (ex. 21.0 and 2.0) from keyboard, using scanf.
+c) Prints the selected operator on the screen, followed by the result of the operation (for 
+example: / 10.50).
+Careful: What happens if op2 is 0? How do you handle the problem?
+*/
+
+   
