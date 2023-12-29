bcd57b48494babe547ffab7fd1b3bee03b5b40ce
Borzu <74207166+borzoian40@users.noreply.github.com>
2023-12-29 23:35:37 +0100

    05.c

--- LAB1/05.c
+++ LAB1/05.c
@@ -8,5 +8,57 @@ c) Prints the selected operator on the screen, followed by the result of the ope
 example: / 10.50).
 Careful: What happens if op2 is 0? How do you handle the problem?
 */
+#include <stdio.h>
+#include <math.h>
 
-   
+int main() {
+    char operator;
+    float op1, op2, result;
+
+    //Step a: Acquire Operator
+    printf("Enter operator (+ - * /): ");
+    operator = getchar();
+
+    //Step b: Acquire two float operands
+    printf("Enter operand 1: ");
+    scanf("%f", &op1);
+
+    printf("Enter operand 2: ");
+    scanf("%f", &op2);
+
+    //Step C: Perform the selected operation and handle division by zero
+
+    switch (operator) {
+        case '+':
+            result = op1 + op2;
+            printf("%.2f  %c  %.2f  = %.2f\n", op1, operator, op2, result);
+            break;
+
+        case '-':
+            result = op1 - op2;
+            int final_Result = fabsf(result);
+            printf("%.2f  %c  %.2f = %.2f\n", op1, operator, op2, final_Result);
+            break;
+
+        case '*':
+            result = op1 * op2;
+            printf("%.2f  %c  %.2f = %.2f\n", op1, operator, op2, result);
+            break;
+
+        case '/':
+            if (op2 != 0){
+                result = op1/op2;
+                printf("%.2f  %c %.2f = %.2f\n", op1, operator, op2, result);
+            }
+            else{
+                printf("Division by zero is not allowed\n");
+            }
+            break;
+        default:
+            printf("Invalid Operations\n");
+            return 1;
+
+    }
+    return 0;
+
+}
